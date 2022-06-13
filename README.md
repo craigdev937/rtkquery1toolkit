e2c68c8faa2430a911245d4b26d12a9d16558a49
Craig <indigomx9@gmail.com>
2022-06-13 18:15:20 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# RTK Query
+
+Inital setup of the project.  Created with React, Vite, React Router, and Redux-Toolkit.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur asperiores reiciendis beatae voluptatibus optio soluta minima neque dicta nulla ad mollitia, blanditiis commodi. Repellat, deserunt molestias. Enim doloremque illum error?</p>
+        </React.Fragment>
+    );
+};
+
+
